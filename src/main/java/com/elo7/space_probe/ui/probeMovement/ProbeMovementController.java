13fc4f19abf9862aa72f0c490366f9b8f1ae8c03
reisvictor1 <reisvictor448@gmail.com>
2024-10-09 09:34:13 -0300

    :sparkles: handle collision

--- src/main/java/com/elo7/space_probe/ui/probeMovement/ProbeMovementController.java
+++ src/main/java/com/elo7/space_probe/ui/probeMovement/ProbeMovementController.java
@@ -1,5 +1,7 @@
 package com.elo7.space_probe.ui.probeMovement;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,17 +9,22 @@ import com.elo7.space_probe.domain.Probe;
 import com.elo7.space_probe.domain.Planet;
 import com.elo7.space_probe.ui.probes.ProbeDTO;
 import com.elo7.space_probe.ui.planets.PlanetDTO;
-import com.elo7.space_probe.app.planets.FindAllPlanetService;
 import com.elo7.space_probe.app.planets.FindPlanetService;
 import com.elo7.space_probe.app.probes.FindAllProbeService;
 import com.elo7.space_probe.app.probes.FindProbeService;
+import com.elo7.space_probe.ui.ErrorMessageDTO;
+import com.elo7.space_probe.ui.probeMovement.ProbeMovementInputException;
+import com.elo7.space_probe.ui.GlobalExceptionHandler;
 
 import com.elo7.space_probe.app.probeMovement.UpdateProbePositionService;
+
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import com.elo7.space_probe.ui.planets.PlanetDTO;
 
@@ -25,22 +32,32 @@ import com.elo7.space_probe.ui.planets.PlanetDTO;
 @RequestMapping("v1/movement")
 class ProbeMovementController {
   private final FindProbeService findProbeService;
+  private final FindAllProbeService findAllProbeService;
   private final FindPlanetService findPlanetService;
   private final ProbeMovementToDtoConverter probeMovementToDtoConverter;
   private final UpdateProbePositionService updateProbePositionService;
+  private final GlobalExceptionHandler globalExceptionHandler;
 
-  ProbeMovementController(FindProbeService findProbeService, FindPlanetService findPlanetService, ProbeMovementToDtoConverter probeMovementToDtoConverter, UpdateProbePositionService updateProbePositionService) {
+  ProbeMovementController(FindProbeService findProbeService, FindAllProbeService findAllProbeService, FindPlanetService findPlanetService, ProbeMovementToDtoConverter probeMovementToDtoConverter, UpdateProbePositionService updateProbePositionService, GlobalExceptionHandler globalExceptionHandler) {
     this.findProbeService = findProbeService;
     this.findPlanetService = findPlanetService;
     this.probeMovementToDtoConverter = probeMovementToDtoConverter;
     this.updateProbePositionService = updateProbePositionService;
+    this.findAllProbeService = findAllProbeService;
+    this.globalExceptionHandler = globalExceptionHandler;
   }
 
   @ResponseStatus(HttpStatus.OK)
   @PutMapping("/{id}")
-  ProbeDTO update(@PathVariable("id") Integer id, @RequestBody ProbeMovementDTO probeMovementDTO){
+  ProbeDTO update(@PathVariable("id") Integer id, @RequestBody ProbeMovementDTO probeMovementDTO) throws ProbeMovementInputException{
+    
+    char[] moves = probeMovementDTO.move().toCharArray();
     Probe probe = findProbeService.execute(id).orElse(null);
+
     PlanetDTO planet = findPlanetService.execute(probe.getPlanetId()).map(probeMovementToDtoConverter::convertPlanet).orElse(null);
+
+    List<Probe> probesResponse = findAllProbeService.execute();
+    List<ProbeDTO> probes = probesResponse.stream().filter(p -> p.getPlanetId() == probe.getPlanetId()).map(probeMovementToDtoConverter::convertProbe).toList();
 
     Integer x = probe.getXPosition();
     Integer y = probe.getYPosition();
@@ -48,14 +65,21 @@ class ProbeMovementController {
     Integer planet_x_size = planet.width();
     Integer planet_y_size = planet.height();
 
-    char[] moves = probeMovementDTO.move().toCharArray();
+    
 
     for (int i = 0; i < moves.length; i++) {
       switch (probe_direction) {
           case "N" -> {
             switch(moves[i]) {
               case 'M' -> {
-                if(x - 1 >= 0) x = x - 1;
+                if(x - 1 >= 0) {
+                  for(int j = 0; j < probes.size(); j++) {
+                    ProbeDTO temp_probe = probes.get(j);
+                    if(temp_probe.x() == x - 1) throw new ProbeMovementInputException("Probe Collision");
+                  }
+                 
+                  x = x - 1;
+                }
               }
               case 'L' -> {
                 probe_direction = "W";
@@ -64,12 +88,19 @@ class ProbeMovementController {
               case 'R' -> {
                 probe_direction = "E";
               }
+              default -> throw new ProbeMovementInputException("Invalid Move");
             }
           }
           case "W" -> {
             switch(moves[i]) {
               case 'M' -> {
-                if(y - 1 >= 0) y = y - 1;
+                if(y - 1 >= 0) {
+                  for(int j = 0; j < probes.size(); j++) {
+                    ProbeDTO temp_probe = probes.get(j);
+                    if(temp_probe.y() == y - 1) throw new ProbeMovementInputException("Probe Collision");
+                  }
+                  y = y - 1;
+                } 
               }
               case 'L' -> {
                 probe_direction = "S";
@@ -78,13 +109,20 @@ class ProbeMovementController {
               case 'R' -> {
                 probe_direction = "N";
               }
+              default -> throw new ProbeMovementInputException("Invalid Move");
             }
           }
 
           case "S" -> {
              switch(moves[i]) {
               case 'M' -> {
-                if(x + 1 < planet_x_size) x = x + 1;
+                if(x + 1 < planet_x_size) {
+                  for(int j = 0; j < probes.size(); j++) {
+                    ProbeDTO temp_probe = probes.get(j);
+                    if(temp_probe.x() == x + 1) throw new ProbeMovementInputException("Probe Collision");
+                  }
+                  x = x + 1;
+                }  
               }
               case 'L' -> {
                 probe_direction = "S";
@@ -93,13 +131,20 @@ class ProbeMovementController {
               case 'R' -> {
                 probe_direction = "N";
               }
+              default -> throw new ProbeMovementInputException("Invalid Move");
             }
           }
 
           case "E" -> {
              switch(moves[i]) {
               case 'M' -> {
-                if(y + 1 < planet_y_size) y = y + 1;
+                if(y + 1 < planet_y_size) {
+                  for(int j = 0; j < probes.size(); j++) {
+                    ProbeDTO temp_probe = probes.get(j);
+                    if(temp_probe.y() == y + 1) throw new ProbeMovementInputException("Probe Collision");
+                  }
+                  y = y + 1;
+                } 
               }
               case 'L' -> {
                 probe_direction = "N";
@@ -108,6 +153,7 @@ class ProbeMovementController {
               case 'R' -> {
                 probe_direction = "S";
               }
+              default -> throw new ProbeMovementInputException("Invalid Move");
             }
           }
           default ->  throw new AssertionError();
---
+++ src/main/java/com/elo7/space_probe/ui/probeMovement/ProbeMovementInputException.java
@@ -0,0 +1,10 @@
+package com.elo7.space_probe.ui.probeMovement;
+
+
+public class ProbeMovementException extends Exception {
+
+  public ProbeMovementException(String message){
+     super(message);
+  }
+
+}
